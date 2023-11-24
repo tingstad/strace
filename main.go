b49c7f7e53b5cc49387131ed84d8e4e1cc424c4f
Richqrd <richard.tingstad@gmail.com>
2023-11-24 23:25:53 +0100

    write

--- main.go
+++ main.go
@@ -102,6 +102,9 @@ func main() {
 				// void * mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset)
 				str += fmt.Sprintf(` (%d, %d, %d, %d, %d, %d)`,
 					arg1, arg2, arg3, arg4, arg5, arg6)
+			case syscall.SYS_WRITE:
+				// ssize_t write(int fd, const void *buf, size_t count)
+				str += fmt.Sprintf(` (%d, %d, %d)`, arg1, arg2, arg3)
 			}
 
 			state := "PRE"

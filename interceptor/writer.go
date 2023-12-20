40422e4d17e71bb9498726537e51d1ebb30f4a4a
Richqrd <richard.tingstad@gmail.com>
2023-12-20 21:55:07 +0100

    split Before & After writing

--- interceptor/writer.go
+++ interceptor/writer.go
@@ -10,15 +10,57 @@ import (
 
 type writer struct {
 	provider Provider
+	temp     string
 }
 
 func NewWriter(provider Provider) *writer {
-	return &writer{provider}
+	return &writer{provider, ""}
 }
 
 func (w *writer) Before(syscallNum, arg1, arg2, arg3, arg4, arg5, arg6 int) {
 	syscallName := strings.ToLower(syscalls.GetName(syscallNum))
+
 	fmt.Printf("%s", syscallName)
+
+	str := ""
+
+	switch syscallNum {
+	case syscall.SYS_GETUID, syscall.SYS_GETEUID:
+		// uid_t get[e]uid(void)
+		str += fmt.Sprintf(`() `)
+	case syscall.SYS_OPEN:
+		// int open(const char *path, int oflag, ...)
+		path := w.provider.ReadPtraceText(uintptr(arg1))
+		w.temp = path
+		str += fmt.Sprintf(`("%s", %d) `, path, arg2)
+	case syscall.SYS_READ:
+		// ssize_t read(int fildes, void *buf, size_t nbyte)
+		fd := formatFileDesc(arg1, w.provider.FileName(arg1))
+		str += fmt.Sprintf("(%s, %d, %d)\n", fd, arg2, arg3)
+	case syscall.SYS_LSEEK:
+		// off_t lseek(int fildes, off_t offset, int whence)
+		// If whence is SEEK_SET, the file offset shall be set to offset bytes.
+		// If whence is SEEK_CUR, the file offset shall be set to its current location plus offset.
+		// If whence is SEEK_END, the file offset shall be set to the size of the file plus offset.
+		// https://pubs.opengroup.org/onlinepubs/009696799/functions/lseek.html
+		fd := formatFileDesc(arg1, w.provider.FileName(arg1))
+		whence := map[int]string{0: "SEEK_SET", 1: "SEEK_CUR", 2: "SEEK_END"}
+		str += fmt.Sprintf(`(%s, %d, %s) `, fd, arg2, whence[arg3])
+	case syscall.SYS_MMAP:
+		// void * mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset)
+		str += fmt.Sprintf("(%d, %d, %d, %d, %d, %d)\n",
+			arg1, arg2, arg3, arg4, arg5, arg6)
+	case syscall.SYS_WRITE:
+		// ssize_t write(int fd, const void *buf, size_t count)
+		buf := shortString(w.provider.ReadPtraceTextBuf(uintptr(arg2), arg3))
+		str += fmt.Sprintf(`(%d, %q, %d) `, arg1, buf, arg3)
+	case syscall.SYS_STAT:
+		// int stat(const char *restrict pathname, struct stat *restrict statbuf)
+		path := w.provider.ReadPtraceText(uintptr(arg1))
+		str += fmt.Sprintf(`(%s, %d) `, path, arg2)
+	}
+
+	fmt.Printf("%s", str)
 }
 
 func (w *writer) After(syscallNum, arg1, arg2, arg3, arg4, arg5, arg6, retVal int) {
@@ -28,43 +70,30 @@ func (w *writer) After(syscallNum, arg1, arg2, arg3, arg4, arg5, arg6, retVal in
 	switch syscallNum {
 	case syscall.SYS_GETUID, syscall.SYS_GETEUID:
 		// uid_t get[e]uid(void)
-		str += fmt.Sprintf(`() = %d`, retVal)
+		str += fmt.Sprintf(`= %d`, retVal)
 	case syscall.SYS_OPEN:
 		// int open(const char *path, int oflag, ...)
-		path := w.provider.ReadPtraceText(uintptr(arg1))
+		path := w.temp
 		fd := retVal
-		str += fmt.Sprintf(`("%s", %d) = %d`, path, arg2, fd)
+		str += fmt.Sprintf(`= %d`, fd)
 		w.provider.PutFileDescriptor(fd, path)
 	case syscall.SYS_READ:
 		// ssize_t read(int fildes, void *buf, size_t nbyte)
-		fd := formatFileDesc(arg1, w.provider.FileName(arg1))
 		if retVal <= arg3 {
 			buf := shortString(w.provider.ReadPtraceTextBuf(uintptr(arg2), retVal))
-			str += fmt.Sprintf(`(%s, %d, %d) = %d: %s`, fd, arg2, arg3, retVal, buf)
+			str += fmt.Sprintf(`= %d: %s`, retVal, buf)
 		} else {
-			str += fmt.Sprintf(`(%s, %d, %d) = %d`, fd, arg2, arg3, retVal)
+			str += fmt.Sprintf(`= %d`, retVal)
 		}
 	case syscall.SYS_LSEEK:
 		// off_t lseek(int fildes, off_t offset, int whence)
-		// If whence is SEEK_SET, the file offset shall be set to offset bytes.
-		// If whence is SEEK_CUR, the file offset shall be set to its current location plus offset.
-		// If whence is SEEK_END, the file offset shall be set to the size of the file plus offset.
-		// https://pubs.opengroup.org/onlinepubs/009696799/functions/lseek.html
-		fd := formatFileDesc(arg1, w.provider.FileName(arg1))
-		whence := map[int]string{0: "SEEK_SET", 1: "SEEK_CUR", 2: "SEEK_END"}
-		str += fmt.Sprintf(`(%s, %d, %s) = %d`, fd, arg2, whence[arg3], retVal)
-	case syscall.SYS_MMAP:
-		// void * mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset)
-		str += fmt.Sprintf(`(%d, %d, %d, %d, %d, %d)`,
-			arg1, arg2, arg3, arg4, arg5, arg6)
+		str += fmt.Sprintf(`= %d`, retVal)
 	case syscall.SYS_WRITE:
 		// ssize_t write(int fd, const void *buf, size_t count)
-		buf := shortString(w.provider.ReadPtraceTextBuf(uintptr(arg2), arg3))
-		str += fmt.Sprintf(`(%d, %q, %d)`, arg1, buf, arg3)
+		str += fmt.Sprintf(`= %d`, retVal)
 	case syscall.SYS_STAT:
 		// int stat(const char *restrict pathname, struct stat *restrict statbuf)
-		path := w.provider.ReadPtraceText(uintptr(arg1))
-		str += fmt.Sprintf(`(%s, %d) = %d`, path, arg2, retVal)
+		str += fmt.Sprintf(`= %d`, retVal)
 	}
 
 	fmt.Printf("%s\n", str)
